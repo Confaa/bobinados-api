3150d5e555bbdc279244d864bdc5d88e93e0ceb8
Confaa <juanconfaa@gmail.com>
2026-02-19 11:44:09 -0300

    [bugfix] Fixs connection db

--- core/database.py
+++ core/database.py
@@ -5,7 +5,7 @@ POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 
-postgres_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}"
+postgres_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@bobinados-db:5432/{POSTGRES_DB}"
 
 engine = create_engine(postgres_url)
 
